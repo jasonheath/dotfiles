462c883a826dbc30eca92221f89713453dfc1541
Jason Heath <jh@jasonheath.com>
2024-08-08 12:50:38 -0400

    Configs nvim/init.lua with considered values

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,21 +1,16 @@
---vim.opt.autowrite = true
---vim.opt.confirm = true
---vim.opt.expandtab = true
---vim.opt.fileformats = 'unix'
---vim.opt.ignorecase = true
---vim.opt.lazyredraw = true
---vim.opt.listchars = { eol = '%', tab = '<->', space = '.', multispace = '...+', nbsp = '_' }
---vim.opt.magic = true
---vim.opt.number = true
---vim.opt.report = 0
---vim.opt.shiftwidth = 2
---vim.opt.showmatch = true
---vim.opt.smartcase = true
---vim.opt.smartindent = true
---vim.opt.splitbelow = true
---vim.opt.splitright = true
---vim.opt.tabstop = 2
---vim.opt.visualbell = true
---vim.opt.warn = true
---vim.opt.wrap = false
---vim.opt.wrapscan = false
+vim.opt.autowrite = true
+vim.opt.confirm = true
+vim.opt.expandtab = true
+vim.opt.fileformats = 'unix'
+vim.opt.ignorecase = true
+vim.opt.listchars = { eol = '%', tab = '<->', space = '.', multispace = '...+', nbsp = '_' }
+vim.opt.number = true
+vim.opt.report = 0
+vim.opt.shiftwidth = 2
+vim.opt.showmatch = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.visualbell = true
+vim.opt.wrap = false
+vim.opt.wrapscan = false

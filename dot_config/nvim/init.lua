aa24a1f3477377be4a01b6ce04646c04cfc2287d
Jason Heath <jh@jasonheath.com>
2024-08-14 13:02:33 -0400

    Configs nvim for recommend tab behaviour and opts out of recommended markdown style that changes tab behavior

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,5 @@
 vim.cmd.colorscheme "torte"
+vim.g.markdown_recommended_style = 0 # https://github.com/neovim/neovim/blob/d25889ab7607918a152bab5ce4d14e54575ec11b/runtime/ftplugin/markdown.vim
 vim.opt.autowrite = true
 vim.opt.confirm = true
 vim.opt.expandtab = true
@@ -11,6 +12,7 @@ vim.opt.shiftwidth = 2
 vim.opt.showmatch = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
-vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 8
 vim.opt.visualbell = true
 vim.opt.wrapscan = false

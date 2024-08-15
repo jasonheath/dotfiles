00fccf20126cd2ae412cbbd38c3b2ca871d8545b
Jason Heath <jh@jasonheath.com>
2024-08-15 17:41:50 -0400

    Configues "/= vertical/horizontal splitting and kills ~/.ssh/config parsing

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -57,5 +57,44 @@ config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font_size = 16.0
 config.enable_tab_bar = false
 
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+config.keys = {
+    { key = '\"', mods = 'ALT|CTRL', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+    { key = '\"', mods = 'SHIFT|ALT|CTRL', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+    { key = '\'', mods = 'SHIFT|ALT|CTRL', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+
+    { key = '=', mods = 'ALT|CTRL', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+    { key = '=', mods = 'SHIFT|ALT|CTRL', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+    { key = '+', mods = 'SHIFT|ALT|CTRL', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+}
+
+-- JAH: From here to config.ssh_domains = ssh_domains cleans up the shell menu
+-- JAH: Leaving all this extra stuff so that I have clues in the future if I want them
+local ssh_domains = {}
+--for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
+--  if host:match("jah") then
+--    table.insert(ssh_domains, {
+--      name = host, -- the name can be anything you want; we're just using the hostname
+--      remote_address = host, -- remote_address must be set to `host` for the ssh config to apply to it
+--
+--      -- if you don't have wezterm's mux server installed on the remote
+--      -- host, you may wish to set multiplexing = "None" to use a direct
+--      -- ssh connection that supports multiple panes/tabs which will close
+--      -- when the connection is dropped.
+--
+--      -- multiplexing = "None",
+--
+--      -- if you know that the remote host has a posix/unix environment,
+--      -- setting assume_shell = "Posix" will result in new panes respecting
+--      -- the remote current directory when multiplexing = "None".
+--      assume_shell = 'Posix',
+--    })
+--  end
+--end
+config.ssh_domains = ssh_domains
+
+
 -- and finally, return the configuration to wezterm
 return config

838af69cbff8fd12aca7db67e34ff160e2e9822b
Jason Heath <jh@jasonheath.com>
2024-08-26 15:21:34 -0400

    adds nvim/lua/plugins/vim-strip-trailing-whitespace.lua

---
+++ dot_config/nvim/lua/plugins/vim-strip-trailing-whitespace.lua
@@ -0,0 +1,5 @@
+-- the advantage of this plugin over others is that it silently strips out
+-- trailing whitespace on lines that you are changes and ignores trailing
+-- linespaces on lines you haven't
+return { 'axelf4/vim-strip-trailing-whitespace' }
+

3707a45794016ae88343d97045e28ba15f49d9df
Jason Heath <jh@jasonheath.com>
2024-08-26 13:46:26 -0400

    Update .config/nvim/init.lua Update .gitconfig

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,7 +5,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.markdown_recommended_style = 0 -- https://github.com/neovim/neovim/blob/d25889ab7607918a152bab5ce4d14e54575ec11b/runtime/ftplugin/markdown.vim
 vim.opt.autowrite = true
 vim.opt.confirm = true
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.fileformats = 'unix'
 vim.opt.ignorecase = true
 vim.opt.listchars = { eol = '%', tab = '<->', space = '.', multispace = '...+', nbsp = '_' }
@@ -15,8 +15,8 @@ vim.opt.shiftwidth = 2
 vim.opt.showmatch = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 8
+--vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 vim.opt.visualbell = true
 vim.opt.wrapscan = false
 

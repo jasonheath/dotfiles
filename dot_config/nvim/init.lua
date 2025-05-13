82b75d508c4cdf222e7b1d1b67f693edd3720f24
Jason Heath <jh@jasonheath.com>
2025-05-13 08:30:31 -0400

    Comments out vim.opt.relativenumber

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.fileformats = 'unix'
 vim.opt.ignorecase = true
 vim.opt.listchars = { eol = '%', tab = '<->', space = '.', multispace = '...+', nbsp = '_' }
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 vim.opt.report = 0
 vim.opt.shiftwidth = 2
 vim.opt.showmatch = true

739b6b9ccee99f23eeb904952e7cc015857b0279
Jason Heath <jh@jasonheath.com>
2026-02-04 10:15:44 -0500

    Adds barebones and likely unnecessary ~/.config/nvim/init.lua
    Signed-off-by: Jason Heath <jh@jasonheath.com>

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- https://github.com/vscode-neovim/vscode-neovim?tab=readme-ov-file#neovim-configuration
+-- I only use NeoVim for vscode-neovim so an empty init file should be fine but
+-- I'm still adding this recommended bit of config as belt and suspenders
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+end
